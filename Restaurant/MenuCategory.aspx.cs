a5340b26b4fce16df63774685fcde6e77e481ea7
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-07-24 15:19:46 +0530

    Creating new project Restuarant

---
+++ Restaurant/MenuCategory.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MenuCategory : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            lbl.Text = "";
+            DataSet1TableAdapters.MenuCategoriesTableAdapter da = new DataSet1TableAdapters.MenuCategoriesTableAdapter();
+            DataSet1.MenuCategoriesDataTable dt = da.CategoryData();
+
+            if (dt.Rows.Count <= 0)
+            {
+                throw new Exception("Invalid Data");
+            }
+
+            // Clear the labels before displaying data
+            lbl.Text = "";
+
+            // Loop through the DataTable to display each row separately
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                DataSet1.MenuCategoriesRow dr = (DataSet1.MenuCategoriesRow)dt.Rows[i];
+                lbl.Text += dr.categoryno + "<br />";
+                lbl.Text += dr.name + "<br />";
+                lbl.Text += dr.comment + "<br /><br />";
+            }
+        }
+        catch (Exception ex)
+        {
+            lbl.Text = ex.Message;
+        }
+    }
+}
---
+++ Restaurant/RestuarantInfo.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class RestuarantInfo : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack) // Check if the page is not loaded due to a postback
+        {
+            try
+            {
+                lbl.Text = "";
+                DataSet1TableAdapters.Restuarant_InfoTableAdapter da = new DataSet1TableAdapters.Restuarant_InfoTableAdapter();
+                DataSet1.Restuarant_InfoDataTable dt = da.RestuarantData();
+
+                if (dt.Rows.Count <= 0)
+                {
+                    throw new Exception("Invalid Data");
+                }
+
+                DataSet1.Restuarant_InfoRow dr = (DataSet1.Restuarant_InfoRow)dt.Rows[0];
+                lblRestuarantName.Text = dr.restuarantname;
+                lblPhoneNo.Text = "" + dr.phoneno;
+                lblAddress.Text = dr.address;
+                lblPincode.Text = "" + dr.pincode;
+                lblTagline.Text = "" + dr.tagline;
+            }
+            catch (Exception ex)
+            {
+                lbl.Text = ex.Message;
+                lblRestuarantName.Text = "";
+                lblPhoneNo.Text = "";
+                lblAddress.Text = "";
+                lblPincode.Text = "";
+                lblTagline.Text = "";
+            }
+        }
+    }
+    
+}
